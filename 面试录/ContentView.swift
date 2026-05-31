3ac9cd5e3c8d0a2dd12b673cd64370e437f535fa
Ori <740879160@qq.com>
2026-05-31 15:54:35 +0800

    Initial Commit

---
+++ 面试录/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  面试录
+//
+//  Created by ori_mac on 2026/5/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
